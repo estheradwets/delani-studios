30ab99285dac0ba804d1e43e26afe7a37b999718
estheradwets <estheradwets00@gmail.com>
2022-03-13 19:53:23 +0300

    Add JS script

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+//   $("#design").click(function(){
+//     $("#design-description").toggle()
+//     $("#design-pic").toggle()
+//   });
+      
+      $(#)
+
+
+
+  $("#development").click(function(){
+    $("#development-description").toggle()
+    $("#dev").toggle()
+  });
+
+  $("#product").click(function(){
+    $("#product-description").toggle()
+    $("#product-pic").toggle()
+  });
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  });
